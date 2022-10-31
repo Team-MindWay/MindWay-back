0f5f5e272bc4053df8c66604f85c74b7f9b06f71
Juhyunho <hyun050301@gmail.com>
2022-10-31 23:06:53 +0900

    Update settings

--- MindWay/settings.py
+++ MindWay/settings.py
@@ -12,11 +12,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os, json
 from pathlib import Path
+import datetime
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# secrets.json get_secrets
 secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
 with open(secret_file) as f:
@@ -30,6 +32,7 @@ def get_secret(setting):
         error_msg = "Set the {} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
 
+# SECRET_KEY
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
@@ -40,7 +43,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -50,7 +52,29 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : (
+        'rest_framework.permissions.AllowAny'
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES' : (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthenticatin',
+    )
+}
+
+JWT_AUTH = {
+    'JWT_SECRET_KEY' : SECRET_KEY,
+    'JWT_ALGORITHM' : 'HS256',
+    'JWT_ALLOW_REFRESH' : True,
+    'JWT_EXPIRATION_DELTA' : datetime.timedelta(minutes=30),
+    'JWT_REFRESH_EXPIRATION_DELTA' : datetime.timedelta(days=28)
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -112,7 +136,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
@@ -128,3 +152,34 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#logging
+DEFAULT_LOGGING = {
+    'version' : 1,
+    'disable_existing_loggers' : False,
+    'loggers' : {
+        '' : {
+            'level' : 'INFO',
+        },
+        'another.module' : {
+            'level' : 'DEBUG',
+        }
+    }
+}
+
+#SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = '587'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = get_secret("SMTP")['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = get_secret("SMTP")['EMAIL_HOST_PASSWORD']
+
+
+# Redis Cache
+CACHE = {
+    'default' : {
+        'BACKEND' : 'django_redis.cache.Redis.Cache',
+        'LOCATION' : 'redis://127.0.0.1:6379'
+    }
+}
