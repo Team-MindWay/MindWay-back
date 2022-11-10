9f82602f887b6e880b992fa843520d7a0fad1fbd
Juhyunho <hyun050301@gmail.com>
2022-11-10 23:54:36 +0900

    비밀번호 변경 로직을 구현했습니다.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('signup', Signup.as_view()),
     path('validation/<str:uid>/<str:token>', Validation.as_view()),
+    path('request/validation', RequestValidation.as_view()),
+    path('password/change', ChangePassword.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 import jwt
 from rest_framework import generics, status
 from django.http import JsonResponse
+from django.core.exceptions import ValidationError
 from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_text
-from django.core.exceptions import ValidationError
+from django.shortcuts import redirect
 from django.conf import settings
+from django.core.cache import cache
 
-from .serializers import SignupSerializer
+from .serializers import *
 from .models import *
 from .email import send
 
@@ -41,9 +43,65 @@ class Validation(generics.GenericAPIView):
             if not user.email == user_email['email']:
                 return JsonResponse({'message' : 'Fail'}, status=status.HTTP_400_BAD_REQUEST)
 
+            if user.is_active is True:
+                cache_data = cache.get(user.email)
+                if cache_data == 'False':
+                    cache.set(user.email, 'True', 30)
+
+                return JsonResponse({'message' : 'success'})
+
             user.is_active = True
             user.save()
 
             return JsonResponse({'message' : 'Success'}, status=status.HTTP_200_OK)
         except ValidationError:
             return JsonResponse({'message' : 'Type Error'}, status=status.HTTP_400_BAD_REQUEST)
+
+class RequestValidation(generics.GenericAPIView):
+    def post(self, request):
+        serializer = EmailSerializer(data=request.data)
+
+        if not serializer.is_valid(raise_exception=True):
+            return JsonResponse({'message' : 'Fail'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+
+        if user['email'] == None:
+            return JsonResponse({'message' : 'Fail'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        if user['is_active'] == False:
+            return JsonResponse({'message' : 'Fail'}, status=status.HTTP_403_FORBIDDEN)
+
+        send(request, user['email'])
+        request.session['email'] = user['email']
+
+        if cache.get(user['email']) is None:
+            cache.set(user['email'], 'False', 30)
+        
+        return JsonResponse({'message' : 'Success'})
+
+class ChangePassword(generics.GenericAPIView):
+    def post(self, request):
+        cache_data = cache.get(request.session['email'])
+
+        if cache_data == 'False' or cache_data is None:
+            return JsonResponse({'message' : 'Not Valid'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        password = request.POST['password']
+
+        if not password == request.POST['password_check']:
+            return JsonResponse({'message' : 'Fail'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.get(email=request.session['email'])
+        serializer = ChangePasswordSerializer(user, data={'password' : password})
+
+        if not serializer.is_valid(raise_exception=True):
+            return JsonResponse({'message' : 'Fail'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+
+        cache.delete(request.session['email'])
+        del request.session['email']
+
+        return JsonResponse({'message' : 'Success'})
