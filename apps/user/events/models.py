17ead8aa06c8d8a7f44cfa1cdd9269c94daea43f
Juhyunho <hyun050301@gmail.com>
2023-02-02 19:19:42 +0900

    이벤트 정보 로직 구현

--- apps/user/events/models.py
+++ apps/user/events/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=256)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    content = models.TextField()
+    image = models.ImageField()
+
+    def __str__(self) -> str:
+        return f'{self.title}'
--- settings/user_settings.py
+++ settings/user_settings.py
@@ -3,5 +3,6 @@ from .base import *
 INSTALLED_APPS += [
     'apps.user.accounts',
     'apps.user.applications',
+    'apps.user.events',
 ]
 ROOT_URLCONF = 'MindWay.urls_user'
