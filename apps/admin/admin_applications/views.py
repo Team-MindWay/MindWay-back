fbc8c90d90b743646aff82eb8675b45ee9670eeb
Juhyunho <hyun050301@gmail.com>
2023-02-07 12:09:47 +0900

    admin Library list 로직 구현

--- apps/admin/admin_applications/views.py
+++ apps/admin/admin_applications/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf import settings
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from apps.user.accounts.token import user_valid
+from .serializers import *
+from .models import *
+
+import logging
+import logging.config
+
+logging.config.dictConfig(settings.DEFAULT_LOGGING)
+
+class AdminLibraryApplication(APIView):
+    def get(self, request):
+        user_valid(request)
+        book = Book.objects.all()
+        serializer = LibrarySerializer(book, many=True)
+
+        return Response(serializer.data)
