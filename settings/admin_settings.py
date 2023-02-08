b328fca317156c88d5a629eede42da9bb9a05ac0
Juhyunho <hyun050301@gmail.com>
2023-02-09 00:59:41 +0900

    admin_event app 생성 및 설정

--- settings/admin_settings.py
+++ settings/admin_settings.py
@@ -3,5 +3,6 @@ from .user_settings import *
 INSTALLED_APPS += [
     'apps.admin.admin_accounts',
     'apps.admin.admin_applications',
+    'apps.admin.admin_events',
 ]
 ROOT_URLCONF = 'MindWay.urls_admin'
