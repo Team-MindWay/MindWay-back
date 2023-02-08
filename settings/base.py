bb058ecc8c9fe7670031729e8df0da938dc963c3
Juhyunho <hyun050301@gmail.com>
2023-02-08 14:15:45 +0900

    Update settings base.py

--- settings/base.py
+++ settings/base.py
@@ -189,3 +189,15 @@ CACHE = {
         'LOCATION' : 'redis://127.0.0.1:6379'
     }
 }
+
+AWS_ACCESS_KEY_ID = get_secret("S3")['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = get_secret("S3")['AWS_SECRET_ACCESS_KEY']
+AWS_REGION = 'ap-northeast-2'
+
+AWS_STORAGE_BUCKET_NAME = get_secret("S3")['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'path/to/store/my/files/')
