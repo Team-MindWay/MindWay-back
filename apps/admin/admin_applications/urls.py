29d2e2ae64f0afedf77ba9d7a71cd0a79fd4b2d8
Juhyunho <hyun050301@gmail.com>
2023-03-15 20:15:46 +0900

    delete LibraryApplication
    도서실 신청 기능을 삭제했습니다.

--- apps/admin/admin_applications/urls.py
+++ apps/admin/admin_applications/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('library', AdminLibraryApplication.as_view()),
     path('book', AdminBookApplication.as_view()),
 ]
--- apps/user/applications/serializers.py
+++ apps/user/applications/serializers.py
@@ -6,17 +6,3 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('id', 'user', 'title', 'author', 'url')
-
-class MemberSerializer(serializers.ModelSerializer):
-    student = serializers.StringRelatedField()
-
-    class Meta:
-        model = TeamMember
-        fields = ('team', 'student')
-
-class LibrarySerializer(serializers.ModelSerializer):
-    student = MemberSerializer(many=True, required=False)
-
-    class Meta:
-        model = Library
-        fields = ('id', 'team', 'room', 'student')
