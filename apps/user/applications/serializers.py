0506d5cf4d99488cefeb74a25c44311a45bb4c24
Juhyunho <hyun050301@gmail.com>
2023-03-16 23:12:01 +0900

    add RecommendSerializer

--- apps/user/applications/serializers.py
+++ apps/user/applications/serializers.py
@@ -7,7 +7,22 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = ('id', 'user', 'title', 'author', 'url')
 
+class BookInfoSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Book
+        fields = ('id', 'user', 'title', 'author', 'url')
+
+    def get_user(self, obj):
+        return {'id' : obj.user.id, 'number' : obj.user.number, 'username' : obj.user.username}
+    
 class RecommendSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recommend
+        fields = ('id', 'recommender', 'title', 'author', 'outline')
+
+class RecommendInfoSerializer(serializers.ModelSerializer):
     recommender = serializers.SerializerMethodField()
 
     class Meta:
