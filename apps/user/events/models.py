a5059c146468c8a44995f9699e8bd30c4c6bff27
Juhyunho <hyun050301@gmail.com>
2023-06-20 17:48:13 +0900

    Event 모델과 EventImage 모델 분리

--- apps/user/events/models.py
+++ apps/user/events/models.py
@@ -6,7 +6,13 @@ class Event(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     content = models.TextField()
-    image = models.ImageField()
 
     def __str__(self) -> str:
         return f'{self.title}'
+    
+class EventImage(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    image = models.ImageField()
+
+    def __str__(self) -> str:
+        return f'{self.event.title} image'
--- apps/user/events/serializers.py
+++ apps/user/events/serializers.py
@@ -3,8 +3,19 @@ from .models import *
 
 class EventSerializer(serializers.ModelSerializer):
     title = serializers.CharField()
-    image = serializers.ImageField(use_url=True)
 
     class Meta:
         model = Event
-        fields = ('id', 'title', 'start_date', 'end_date', 'content', 'image')
+        fields = ('id', 'title', 'start_date', 'end_date', 'content')
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventImage
+        fields = ('event', 'image')
+
+class EventGetSerializer(serializers.ModelSerializer):
+    event_image = ImageSerializer()
+
+    class Meta:
+        model = Event
+        fields = ('id', 'title', 'start_date', 'end_date', 'content', 'event_image')
